126283bf98bc2bfd1e8b8dfc0cbae28881385c18
Bobbylee007 <bobbyleeaustine@gmail.com>
2026-03-25 10:34:00 +0100

    root dirctory chaned

---
+++ Public/Js/main.js
@@ -0,0 +1,9 @@
+let Me = {
+  name: "Chukwufumnaya Augustine Osamor",
+  code: ["Javascript", "Shell", "java"],
+  Web: ["React", "Node.js", "Typescript"],
+  Design: ["Figma", "Photoshop","Adobe Ai"],
+  Devops: ["Git","Docker"],
+  Deployment: ["Aws","Heroku","Render","vercel"],
+  Message: "Stay focus and trust your instinct",
+};
